93b05d0258d2dfc4c450aca01b3ca29531e993b4
Autar <asautar@avans.nl>
2019-04-10 20:09:02 +0200

    started with right selected value

--- Portal/src/app/pages/dispenser/dispenser-edit/dispenser-edit.component.ts
+++ Portal/src/app/pages/dispenser/dispenser-edit/dispenser-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-dispenser-edit',
@@ -7,11 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DispenserEditComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
+    this.selectedDispenserId = +this.route.snapshot.paramMap.get('id');
+    console.log(this.selectedDispenserId);
   }
 
   dispenserOptions = [ "hoofdtoediener", "toediener"];
+  selectedDispenserId;
+  selectedDispenser = {
+    "id" : 1,
+    "name" : "Arjun Autar",
+    "email" : "asautar@avans.nl",
+    "role" : "hoofdverzorger"
+  }; 
 
+  testValue = true;
 }

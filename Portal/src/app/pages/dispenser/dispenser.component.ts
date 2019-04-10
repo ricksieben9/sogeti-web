0d5dc3eacb0743257777cac7646872a14cc6ec47
Autar <asautar@avans.nl>
2019-04-10 13:23:21 +0200

    index page of dispensers

--- Portal/src/app/pages/dispenser/dispenser.component.ts
+++ Portal/src/app/pages/dispenser/dispenser.component.ts
@@ -12,4 +12,31 @@ export class DispenserComponent implements OnInit {
   ngOnInit() {
   }
 
+  dispensers = [ 
+    {
+    "id" : 1,
+    "name" : "Arjun Autar",
+    "email" : "asautar@avans.nl",
+    "role" : "hoofdverzorger"
+  },
+  {
+    "id" : 2,
+    "name" : "Arjun Autar",
+    "email" : "asautar@avans.nl",
+    "role" : "hoofdverzorger"
+  },
+  {
+    "id" : 3,
+    "name" : "Kenny Tu",
+    "email" : "ktu@avans.nl",
+    "role" : "hoofdverzorger"
+  },
+  {
+    "id" : 4,
+    "name" : "Kenny Tu",
+    "email" : "ktu@avans.nl",
+    "role" : "hoofdverzorger"
+  }
+]
+
 }

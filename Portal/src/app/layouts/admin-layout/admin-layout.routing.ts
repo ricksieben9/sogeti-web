842de6664e3ced22873c3e7f3b2e4bdb5ed0ca47
Rick van den Heuvel <Rick.heuvel@outlook.com>
2019-04-10 12:25:56 +0200

    added receivers component+Routing

--- Portal/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ Portal/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -5,11 +5,13 @@ import { UsersComponent } from '../../pages/users/users.component';
 import { MapsComponent } from '../../pages/maps/maps.component';
 import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
 import { TablesComponent } from '../../pages/tables/tables.component';
+import {ReceiversComponent} from '../../pages/receivers/receivers.component';
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'dashboard',      component: DashboardComponent },
     { path: 'user-profile',   component: UserProfileComponent },
     { path: 'tables',         component: TablesComponent },
     { path: 'users',          component: UsersComponent },
-    { path: 'maps',           component: MapsComponent }
+    { path: 'maps',           component: MapsComponent },
+    {path: 'receivers',       component: ReceiversComponent}
 ];
---
+++ Portal/src/app/pages/receivers/receivers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receivers',
+  templateUrl: './receivers.component.html',
+  styleUrls: ['./receivers.component.scss']
+})
+export class ReceiversComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

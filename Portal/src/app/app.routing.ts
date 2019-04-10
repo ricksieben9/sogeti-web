0c03de0c1e923f6f95653f0ebe79236f0061a654
Autar <asautar@avans.nl>
2019-04-10 19:20:30 +0200

    id in url detailView

--- Portal/src/app/app.routing.ts
+++ Portal/src/app/app.routing.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
   { path: 'dispensers/createDispenser', 
     component: CreateDispenserComponent 
   }, 
-  { path: 'dispensers/editDispenser', 
+  { path: 'dispensers/editDispenser/:id', 
     component: DispenserEditComponent 
   }, {
     path: '',

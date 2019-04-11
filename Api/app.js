b48a24919752f21b3b7deec298c6936f182d2f28
Autar <asautar@avans.nl>
2019-04-11 17:23:43 +0200

    Revert "Models to js"
    This reverts commit d38e5ba2b46011de6a1d54603a021d0e597cd83e.

--- Api/app.js
+++ Api/app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mysql = require('mysql');
+var mysql= require('mysql');
 var http = require('http');
 
 
@@ -12,33 +12,6 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
-// test connection
-var typeorm = require("typeorm");
-var log_category = require("./models/entities/log_category");
-var EntitySchema = typeorm.EntitySchema;
-typeorm.createConnection({
-  type: "mysql",
-  host: "databases.aii.avans.nl",
-  port: 3306,
-  username: "asautar",
-  password: "Ab12345",
-  database: "asautar_db",
-  entities: [
-    new EntitySchema(require("./models/entities/log_category").log_category)
-  ],
-  synchronize: true,
-  logging: false
-}).then(connection => {
-  // here you can start to work with your entities
-  console.log("connection succeeded");
-  var category1 = {
-    name: "toedienmoment"
-  };
-  //var logCategoryRepository = connection.getRepository("log_category");
-
-}).catch(error => console.log(error));
-// test connection
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -52,35 +25,35 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //Database connection
-app.use(function (req, res, next) {
-  global.connection = mysql.createConnection({
-    host: 'databases.aii.avans.nl',
-    user: 'asautar',
-    password: 'Ab12345',
-    database: 'asautar_db'
-  });
-  connection.connect();
-  next();
+app.use(function(req, res, next){
+	global.connection = mysql.createConnection({
+        host     : 'databases.aii.avans.nl',
+        user     : 'asautar',
+        password : 'Ab12345',
+        database : 'asautar_db'
+	});
+	connection.connect();
+	next();
 });
 
 // Add headers
 app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // Pass to next layer of middleware
-  next();
+    // Pass to next layer of middleware
+    next();
 });
 
 app.use('/', index);
@@ -88,7 +61,7 @@ app.use('/api/v1/users', users);
 
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -97,7 +70,7 @@ app.use(function (req, res, next) {
 
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};

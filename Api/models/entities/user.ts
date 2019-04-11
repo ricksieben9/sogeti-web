ca7449c8d7c33bf2266ce69364df40dc40981019
Autar <asautar@avans.nl>
2019-04-11 17:23:32 +0200

    Revert "replace models"
    This reverts commit dce27fb06f87f894e7b82fc1e7e7890aca786944.

---
+++ Api/models/entities/user.ts
@@ -0,0 +1,65 @@
+import {BaseEntity,Column,Entity,Index,JoinColumn,JoinTable,ManyToMany,ManyToOne,OneToMany,OneToOne,PrimaryColumn,PrimaryGeneratedColumn,RelationId} from "typeorm";
+import {roles} from "./roles";
+import {group_dispensers} from "./group_dispensers";
+import {intake_moment} from "./intake_moment";
+import {log} from "./log";
+
+
+@Entity("user",{schema:"asautar_db" } )
+@Index("email_UNIQUE",["email",],{unique:true})
+@Index("fk_Person_Roles_idx",["roles_role",])
+export class user {
+
+   
+    @ManyToOne(type=>roles, roles=>roles.users,{  nullable:false,onDelete: 'NO ACTION',onUpdate: 'NO ACTION' })
+    @JoinColumn({ name:'roles_role'})
+    roles_role:roles | null;
+
+
+    @PrimaryGeneratedColumn({
+        type:"int", 
+        name:"id"
+        })
+    id:number;
+        
+
+    @Column("varchar",{ 
+        nullable:false,
+        unique: true,
+        length:100,
+        name:"email"
+        })
+    email:string;
+        
+
+    @Column("varchar",{ 
+        nullable:false,
+        length:100,
+        name:"password"
+        })
+    password:string;
+        
+
+    @Column("varchar",{ 
+        nullable:false,
+        length:45,
+        name:"name"
+        })
+    name:string;
+        
+
+   
+    @OneToMany(type=>group_dispensers, group_dispensers=>group_dispensers.user_,{ onDelete: 'NO ACTION' ,onUpdate: 'NO ACTION' })
+    group_dispenserss:group_dispensers[];
+    
+
+   
+    @OneToMany(type=>intake_moment, intake_moment=>intake_moment.dispenser,{ onDelete: 'NO ACTION' ,onUpdate: 'NO ACTION' })
+    intake_moments:intake_moment[];
+    
+
+   
+    @OneToMany(type=>log, log=>log.user_,{ onDelete: 'NO ACTION' ,onUpdate: 'NO ACTION' })
+    logs:log[];
+    
+}

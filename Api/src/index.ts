2a4cbdd87a8533d96c28dfd73014d90fd58eabc2
bartkoevoets <yzig_koevoets@hotmail.com>
2019-04-19 16:01:49 +0200

    Removed unneeded header settings

--- Api/src/index.ts
+++ Api/src/index.ts
@@ -6,7 +6,6 @@ import * as bodyParser from "body-parser";
 import * as helmet from "helmet";
 import * as cors from "cors";
 import routes from "./routes";
-import * as socketio from 'socket.io';
 
 //Connects to the Database -> then starts the express
 createConnection()
@@ -20,12 +19,6 @@ createConnection()
         app.use(cors());
         app.use(helmet());
         app.use(bodyParser.json());
-
-        app.use(function (request, response, next) {
-            response.header("Access-Control-Allow-Origin", "*");
-            response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            next();
-        });
 
         //Set all routes from routes folder
         app.use("/", routes);

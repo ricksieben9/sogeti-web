42bb4b160bca56de743f34708a72f71d2ffa5b7c
Kenny Tu <ktu@avans.nl>
2019-04-21 18:28:02 +0200

    code cleaning intakemoments components

--- Portal/src/app/pages/intakemoments/intakemoment-detail/intakemoment-detail.component.ts
+++ Portal/src/app/pages/intakemoments/intakemoment-detail/intakemoment-detail.component.ts
@@ -1,11 +1,11 @@
 import {Component, OnInit, TemplateRef} from '@angular/core';
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import {BsModalService, BsModalRef} from 'ngx-bootstrap/modal';
 import {IntakeMomentService} from '../../../service/intake-moment.service';
 import {ReceiverService} from '../../../service/receiver.service';
 import {Receiver} from '../../../_models/receiver';
-import {IntakeMoment} from '../../../_models/intakeMoment';
-import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
+//import {IntakeMoment} from '../../../_models/intakeMoment';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-intakemoment-detail',
@@ -15,10 +15,15 @@ import { Location } from '@angular/common';
 export class IntakemomentDetailComponent implements OnInit {
 
   intakemoments: any;
+  intakeMoment: IntakeMoment = new IntakeMoment();
+  errorMsg: ErrorMsg = new  ErrorMsg();
+  modalRef: BsModalRef;
 
   constructor(private intakeMomentService: IntakeMomentService,
+              private receiverService: ReceiverService,
               private route: ActivatedRoute,
-              private location: Location) { }
+              private location: Location,
+              private modalService: BsModalService) { }
 
   ngOnInit() {
     this.getIntakeMomentsOfReceiver();
@@ -31,6 +36,83 @@ export class IntakemomentDetailComponent implements OnInit {
                   this.intakemoments = intakemoments; });
   }
 
+  // openModalAddIntakemoment(template: TemplateRef<any>) {
+  //   this.errorMsg = new ErrorMsg();
+  //   this.intakeMoment = new IntakeMoment();
+  //   this.modalRef = this.modalService.show(template);
+  // }
+
+  // openModalEditIntakemoment(template: TemplateRef<any>, rec: IntakeMoment) {
+  //   this.errorMsg = new ErrorMsg();
+  //   this.intakeMoment.id = rec.id;
+  //   // this.intakeMoment.name = rec.name;
+  //   this.modalRef = this.modalService.show(template);
+  // }
+
+  // openModalDeleteIntakemoment(template: TemplateRef<any>, rec: IntakeMoment) {
+  //   this.errorMsg = new ErrorMsg();
+  //   this.intakeMoment.id = rec.id;
+  //  // this.intakeMoment.name = rec.name;
+  //   this.modalRef = this.modalService.show(template);
+  // }
+
+  onSave() {
+    // !this.intakeMoment.name ? this.errorMsg.name = 'Naam vereist' : '';
+    // if (!this.intakeMoment.name) {
+    //   return;
+    // } else {
+    //   this.intakeMomentService.addIntakeMoment(this.intakeMoment).subscribe(res => {
+    //     this.getReceivers();
+    //     this.modalRef.hide();
+    //     console.log(res);
+    //   }, error => {
+    //      console.log(error);
+    //      this.errorMsg.name = error.error['response'];
+    //    });
+    // }
+  }
+
+  onAlter() {
+    // !this.intakeMoment.name ? this.errorMsg.name = 'Naam vereist' : '';
+    // if (!this.intakeMoment.name) {
+    //   return;
+    // } else {
+    //   this.intakeMomentService.updateIntakeMoment(this.intakeMoment).subscribe(res => {
+    //     this.getReceivers();
+    //     this.modalRef.hide();
+    //     console.log(res);
+    //   }, error => {
+    //     console.log(error);
+    //     this.errorMsg.name = error.error['response'];
+    //   });
+    // }
+  }
+
+  // deleteRec(rec: IntakeMoment) {
+  //   console.log(rec);
+  //   this.intakeMomentService.deleteIntakeMoment(rec).subscribe(res => {
+  //     this.getReceivers();
+  //      this.modalRef.hide();
+  //     console.log(res);
+  //   }, error => {
+  //     console.log(error);
+  //     this.errorMsg.name = error.error['response'];
+  //   });
+  // }
+
 }
 
+class IntakeMoment {
+  id: string;
+  intake_start_time: Date;
+  intake_end_time: Date;
+  receiver_id: string;
+  remark: string;
+  priority_number: string;
+  dispenser: string;
+}
+
+class ErrorMsg {
+  name: string;
+}
 
--- Portal/src/app/pages/intakemoments/intakemoments.component.ts
+++ Portal/src/app/pages/intakemoments/intakemoments.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
-import { IntakeMomentService } from '../../service/intake-moment.service';
 import { ReceiverService } from '../../service/receiver.service';
 
 @Component({
@@ -10,102 +8,20 @@ import { ReceiverService } from '../../service/receiver.service';
 })
 export class IntakemomentsComponent implements OnInit {
 
-  list: any;
-  intakeMoment: IntakeMoment = new IntakeMoment();
-  errorMsg: ErrorMsg = new  ErrorMsg();
-  modalRef: BsModalRef;
+  receivers: any;
 
-  constructor(private intakeMomentService: IntakeMomentService, private receiverService: ReceiverService, private modalService: BsModalService) { }
+  constructor(private receiverService: ReceiverService) { }
 
-  openModalAddIntakemoment(template: TemplateRef<any>) {
-    this.errorMsg = new ErrorMsg();
-    this.intakeMoment = new IntakeMoment();
-    this.modalRef = this.modalService.show(template);
-  }
+  ngOnInit() {
+    this.getReceivers();
+  }  
 
-  openModalEditIntakemoment(template: TemplateRef<any>, rec: IntakeMoment) {
-    this.errorMsg = new ErrorMsg();
-    this.intakeMoment.id = rec.id;
-    // this.intakeMoment.name = rec.name;
-    this.modalRef = this.modalService.show(template);
-  }
-
-  openModalDeleteIntakemoment(template: TemplateRef<any>, rec: IntakeMoment) {
-    this.errorMsg = new ErrorMsg();
-    this.intakeMoment.id = rec.id;
-   // this.intakeMoment.name = rec.name;
-    this.modalRef = this.modalService.show(template);
-  }
-
-  //needs to get receivers to look at the planning of a receiver
+  //get receivers, from there you go to their intakemoments-planning
   getReceivers() {
     const receiverObservable = this.receiverService.getAllReceivers();
     receiverObservable.subscribe((userData: any[]) => {
       console.log(userData);
-      this.list = userData;
+      this.receivers = userData;
     });
-  }
-
-  onSave() {
-    // !this.intakeMoment.name ? this.errorMsg.name = 'Naam vereist' : '';
-    // if (!this.intakeMoment.name) {
-    //   return;
-    // } else {
-    //   this.intakeMomentService.addIntakeMoment(this.intakeMoment).subscribe(res => {
-    //     this.getReceivers();
-    //     this.modalRef.hide();
-    //     console.log(res);
-    //   }, error => {
-    //      console.log(error);
-    //      this.errorMsg.name = error.error['response'];
-    //    });
-    // }
-  }
-
-  ngOnInit() {
-    this.getReceivers();
-  }
-
-  onAlter() {
-    // !this.intakeMoment.name ? this.errorMsg.name = 'Naam vereist' : '';
-    // if (!this.intakeMoment.name) {
-    //   return;
-    // } else {
-    //   this.intakeMomentService.updateIntakeMoment(this.intakeMoment).subscribe(res => {
-    //     this.getReceivers();
-    //     this.modalRef.hide();
-    //     console.log(res);
-    //   }, error => {
-    //     console.log(error);
-    //     this.errorMsg.name = error.error['response'];
-    //   });
-    // }
-  }
-
-  deleteRec(rec: IntakeMoment) {
-    console.log(rec);
-    this.intakeMomentService.deleteIntakeMoment(rec).subscribe(res => {
-      this.getReceivers();
-       this.modalRef.hide();
-      console.log(res);
-    }, error => {
-      console.log(error);
-      this.errorMsg.name = error.error['response'];
-    });
-  }
-
-}
-
-class IntakeMoment {
-  id: string;
-  intake_start_time: Date;
-  intake_end_time: Date;
-  receiver_id: string;
-  remark: string;
-  priority_number: string;
-  dispenser: string;
-}
-
-class ErrorMsg {
-  name: string;
+  }  
 }

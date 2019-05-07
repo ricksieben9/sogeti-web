97306e6f33a13a09a116cfe69dd17b9a1253cab7
Anthony Tran <a.tran@student.avans.nl>
2019-05-07 13:12:19 +0200

    refactor application to intake moment

--- Api/src/controllers/IntakeMomentController.ts
+++ Api/src/controllers/IntakeMomentController.ts
@@ -143,6 +143,125 @@ class IntakeMomentController {
         //After all send a 204 (no content, but accepted) response
         res.status(204).send();
     };
+
+    // Get all intake moment by dispenser(mobile)
+    static getAllIntakeMomentsByDispenser = async (req: Request, res: Response) => {
+        const {userId} = res.locals.jwtPayload;
+        const intakeMomentRepository = getRepository(intake_moment);
+        let intakeMoment;
+        try {
+            intakeMoment = await intakeMomentRepository.find({
+                relations: ["receiver_id", "priority_number", "dispenser", "intake_moment_medicines"],
+                where: {dispenser: userId}
+            });
+        } catch (error) {
+            //If not found, send a 404 response
+            res.status(404).send({"response": "Toedienmoment niet gevonden!"});
+            return;
+        }
+
+
+        //Send the intake moment object
+        res.status(200).send(intakeMoment);
+    };
+
+    // Get intake moment detail by id and user (mobile)
+    static getIntakeMomentDetail = async (req: Request, res: Response) => {
+        const {userId} = res.locals.jwtPayload;
+        const id: number = req.params.id;
+        const intakeMomentRepository = getRepository(intake_moment);
+        let intakeMoment;
+        try {
+            intakeMoment = await intakeMomentRepository.find({
+                relations: ["receiver_id", "priority_number", "dispenser", "intake_moment_medicines"],
+                where: {dispenser: userId, id: id}
+            });
+        } catch (error) {
+            //If not found, send a 404 response
+            res.status(404).send({"response": "Toedienmoment gegevens niet gevonden!"});
+            return;
+        }
+        //Send the users object
+        res.status(200).send(intakeMoment);
+    };
+
+    // set intake moment medicine on completed (mobile)
+    static setIntakeMomentMedicineCompleted = async (req: Request, res: Response) => {
+        const id = req.params.id;
+        let {medicine_id, completed_at} = req.body;
+
+        const intakeMomentMedicineRepository = getRepository(intake_moment_medicines);
+        let IntakeMomentMedicine: intake_moment_medicines;
+        try {
+            IntakeMomentMedicine = await intakeMomentMedicineRepository.findOne({
+                intake_moment_id: id,
+                medicine_id: medicine_id.id
+            });
+        } catch (error) {
+            //If not found, send a 404 response
+            res.status(404).send({"response": "Toedienmoment medicijn niet gevonden!"});
+            return;
+        }
+        IntakeMomentMedicine.intake_moment_id = id;
+        IntakeMomentMedicine.completed_at = completed_at.toString();
+        //Delete the original medicine from intakeMoment
+        const intakeMomentMedicinesRepository = getRepository(intake_moment_medicines);
+        try {
+            intakeMomentMedicinesRepository.delete({intake_moment_id: id, medicine_id: medicine_id.id});
+        } catch (error) {
+            res.status(409).send(error);
+            return;
+        }
+        //Try to safe, if fails, that means intake moment medicine already in use
+        try {
+            await intakeMomentMedicineRepository.save(IntakeMomentMedicine);
+        } catch (e) {
+            res.status(409).send({"response": "Bad request!"});
+            return;
+        }
+        //After all send a 204 (no content, but accepted) response
+        res.status(204).send({"response": "Toedienmoment aangepast"});
+    };
+
+    // remove intake moment medicine on completed (mobile)
+    static removeIntakeMomentMedicineCompleted = async (req: Request, res: Response) => {
+        const id = req.params.id;
+        let {medicine_id} = req.body;
+
+        const intakeMomentMedicineRepository = getRepository(intake_moment_medicines);
+        let IntakeMomentMedicine: intake_moment_medicines;
+        try {
+            IntakeMomentMedicine = await intakeMomentMedicineRepository.findOne({
+                intake_moment_id: id,
+                medicine_id: medicine_id.id
+            });
+
+        } catch (error) {
+            //If not found, send a 404 response
+            res.status(404).send({"response": "Toedienmoment medicijn niet gevonden!"});
+            return;
+        }
+        IntakeMomentMedicine.intake_moment_id = id;
+        IntakeMomentMedicine.completed_at = null;
+        //Delete the original medicine from intakeMoment
+        const intakeMomentMedicinesRepository = getRepository(intake_moment_medicines);
+        try {
+            intakeMomentMedicinesRepository.delete({intake_moment_id: id, medicine_id: medicine_id.id});
+        } catch (error) {
+            res.status(409).send(error);
+            return;
+        }
+        //Try to safe, if fails, that means intake moment medicine already in use
+        try {
+            await intakeMomentMedicineRepository.save(IntakeMomentMedicine);
+
+        } catch (e) {
+            res.status(409).send({"response": "Bad request!"});
+            return;
+        }
+        //After all send a 204 (no content, but accepted) response
+        res.status(204).send({"response": "Toedienmoment aangepast"});
+    };
 }
 
 export default IntakeMomentController;
--- Api/src/routes/index.ts
+++ Api/src/routes/index.ts
@@ -3,16 +3,16 @@ import auth from "./auth";
 import user from "./user";
 import receiver from "./receiver";
 import medicine from "./medicine";
-import application from "./application";
 import priority from "./priority";
 import log from "./log";
+import intakeMoment from "./intakeMoment"
 const routes = Router();
 
 routes.use("/auth", auth);
 routes.use("/user", user);
 routes.use("/receiver", receiver);
 routes.use("/medicine", medicine);
-routes.use("/application", application);
 routes.use("/priority", priority);
 routes.use("/log", log);
+routes.use("/intakeMoment", intakeMoment);
 export default routes;
